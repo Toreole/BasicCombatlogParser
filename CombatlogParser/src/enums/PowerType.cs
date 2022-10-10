a3efabefdb6c2a45c16feb1d03b776ddfa631d8d
Toreole <toreole.official@gmail.com>
2022-10-10 14:46:30 +0200

    Init project with two basic enums

---
+++ CombatlogParser/src/enums/PowerType.cs
@@ -0,0 +1,28 @@
+﻿namespace CombatlogParser
+{
+    public enum PowerType
+    {
+        HealthCost = -2,
+        None = -1,
+        Mana = 0,
+        Rage = 1,
+        Focus = 2,
+        Energy = 3,
+        ComboPoints = 4,
+        Runes = 5,
+        RunicPower = 6,
+        SoulShards = 7,
+        LunarPower = 8,
+        HolyPower = 9,
+        Alternate = 10,
+        Maelstrom = 11,
+        Chi = 12,
+        Insanity = 13,
+        Obsolete = 14,
+        Obsolete2 = 15,
+        ArcaneCharges = 16,
+        Fury = 17,
+        Pain = 18,
+        NumPowerTypes = 19, //this is a value in the original PowerType enum, but it should never be used in a combat log.
+    }
+}
---
+++ CombatlogParser/src/enums/SpellSchool.cs
@@ -0,0 +1,45 @@
+﻿namespace CombatlogParser
+{
+    public enum SpellSchool
+    {
+        UNDEFINED = 0,
+        //the basic ones
+        Physical = 1,
+        Holy = 2,
+        Fire = 4,
+        Nature = 8,
+        Frost = 16,
+        Shadow = 32,
+        Arcane = 64,
+
+        //combinations
+        Holystrike = 3,
+        Flamestrike = 5,
+        Radiant = 6,
+        Stormstrike = 9,
+        Holystorm = 10,
+        Firestorm = 12,
+        Froststrike = 16,
+        Holyfrost = 18,
+        Frostfire = 20,
+        Froststorm = 24,
+        Shadowstrike = 33,
+        Twilight = 34,
+        Shadowlame = 36,
+        Plague = 40,
+        Shadowfrost = 48,
+        Spellstrike = 65,
+        Divine = 66,
+        Spellfire = 68,
+        Astral = 72,
+        Spellfrost = 80,
+        Spellshadow = 96,
+        //multi
+        Elemental = 28,
+        Chromatic = 62,
+        Cosmic = 106,
+        Chaos = 124,
+        Magic = 126,
+        TrueChaos = 127 //this one is just named Chaos in wowpedia, but cant exactly have multiple enum values with the same name.
+    }
+}
