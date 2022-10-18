2785f4c9f47a11be116ccada52b33689df09f648
Toreole <toreole.official@gmail.com>
2022-10-18 16:33:41 +0200

    Testing out some SQLite basics, more idea.txt content

--- CombatlogParser/Program.cs
+++ CombatlogParser/Program.cs
@@ -5,6 +5,46 @@
         [STAThread]
         static void Main()
         {
+            SQLitePCL.Batteries.Init();
+            using (var connection = new Microsoft.Data.Sqlite.SqliteConnection("DataSource=hello.db Mode=ReadWriteCreate"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText =
+                    @"
+                        CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT
+                        );
+                    ";
+                command.ExecuteNonQuery();
+
+                var insertDataCommand = connection.CreateCommand();
+                insertDataCommand.CommandText =
+                    @"
+                        INSERT INTO users (id, name)
+                        VALUES( $varID, $varName);
+                    ";
+                insertDataCommand.Parameters.AddWithValue("$varName", "Daniel");
+                insertDataCommand.Parameters.AddWithValue("$varID", 4);
+                insertDataCommand.ExecuteNonQuery();
+
+                var selectCommand = connection.CreateCommand();
+                selectCommand.CommandText = "SELECT name FROM users WHERE id = $varID;";
+                selectCommand.Parameters.AddWithValue("$varID", 3);
+                using (var reader = selectCommand.ExecuteReader())
+                {
+                    if(reader.Read())
+                    {
+                        var name = reader.GetString(0);
+
+                        bool correct = name == "Hubert";
+                    }
+                }
+
+                connection.Close();
+            }
+            return;
             MainWindow window = new();
             window.ShowDialog();
         }
