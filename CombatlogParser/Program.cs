1face2a809b12d3f7910da2168e14042608c7ad0
Toreole <toreole.official@gmail.com>
2022-11-05 19:53:51 +0100

    Querying for players by name

--- CombatlogParser/Program.cs
+++ CombatlogParser/Program.cs
@@ -2,19 +2,28 @@
 {
     public class Program
     {
-        private static readonly string APP_VERSION = "0.1.0a";
+        private const string APP_VERSION = "0.1.0a";
 
         [STAThread]
         static void Main()
         {
-            //bool updated = Config.Default.AppVersion != APP_VERSION; - could check for app version changed, but what do?
+            bool updated = Config.Default.AppVersion != APP_VERSION; //- could check for app version changed, but what do?
             DB.InitializeConnection();
             DB.SetupDataTables();
+            if (updated)
+            { 
+                //when a change in version is detected, try to upgrade the DB schema to match the latest.
+                DB.Upgrade();
+                //update the AppVersion.
+                Config.Default.AppVersion = APP_VERSION;
+                Config.Default.Save();
+            }
+            //this is where the main application runs.
+            MainWindow app = new();
+            app.ShowDialog();
 
+            //propertly shut down the DB before exiting.
             DB.Shutdown();
-            return;
-            MainWindow window = new();
-            window.ShowDialog();
         }
     }
 }
---
+++ CombatlogParser/src/DBInteract/PlayerQueries.cs
@@ -0,0 +1,34 @@
+﻿using CombatlogParser.Data.Metadata;
+
+namespace CombatlogParser
+{
+    public static partial class Queries
+    {
+        /// <summary>
+        /// Gets the stored Metadata for all players whose names start with the provided string. This should be case-insenstive.
+        /// </summary>
+        public static PlayerMetadata[] AllPlayersWithNameLike(string start)
+        {
+            using var command = DB.CreateCommand();
+            if (command == null)
+                return Array.Empty<PlayerMetadata>();
+            command.CommandText = "SELECT playerGUID, name, realm, classID FROM Player_Metadata WHERE name LIKE $s%";
+            command.Parameters.AddWithValue("$s", start);
+
+            List<PlayerMetadata> results = new(10);
+
+            using var reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                results.Add(new PlayerMetadata()
+                {
+                    GUID = reader.GetString(0),
+                    name = reader.GetString(1),
+                    realm = reader.GetString(2),
+                    classID = reader.GetByte(3)
+                });
+            }
+            return results.ToArray();
+        }
+    }
+}
