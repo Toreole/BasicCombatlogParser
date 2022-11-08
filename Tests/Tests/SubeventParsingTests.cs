a2e3ccd9a18df03f60b41e910ab643f8cb2e5a71
Toreole <toreole.official@gmail.com>
2022-11-09 00:05:25 +0100

    Updated event affix/suffix parsing tests
    They now all use the fast version, also stumbled upon Assert.Fail/Assert.Pass being MASSIVELY SLOWER than Assert.That which feels so wrong, like huh, why is the test 20ms slower because of such a small change?

--- Tests/Tests/SubeventParsingTests.cs
+++ Tests/Tests/SubeventParsingTests.cs
@@ -9,7 +9,7 @@ namespace CombatlogParser.Tests
         {
             string subevent = "SPELL_DAMAGE";
 
-            if (ParsingUtil.TryParsePrefixAffixSubevent(subevent, out CombatlogEventPrefix prefix, out CombatlogEventSuffix suffix))
+            if (ParsingUtil.TryParsePrefixAffixSubeventF(subevent, out CombatlogEventPrefix prefix, out CombatlogEventSuffix suffix))
             {
                 Assert.Multiple(() =>
                 {
@@ -28,7 +28,7 @@ namespace CombatlogParser.Tests
         {
             string subevent = "SPELL_AURA_APPLIED_DOSE";
 
-            if (ParsingUtil.TryParsePrefixAffixSubevent(subevent, out CombatlogEventPrefix prefix, out CombatlogEventSuffix suffix))
+            if (ParsingUtil.TryParsePrefixAffixSubeventF(subevent, out CombatlogEventPrefix prefix, out CombatlogEventSuffix suffix))
             {
                 Assert.Multiple(() =>
                 {
@@ -47,7 +47,7 @@ namespace CombatlogParser.Tests
         {
             string subevent = "SPELL_PERIODIC_DAMAGE";
 
-            if (ParsingUtil.TryParsePrefixAffixSubevent(subevent, out CombatlogEventPrefix prefix, out CombatlogEventSuffix suffix))
+            if (ParsingUtil.TryParsePrefixAffixSubeventF(subevent, out CombatlogEventPrefix prefix, out CombatlogEventSuffix suffix))
             {
                 Assert.Multiple(() =>
                 {
@@ -66,7 +66,7 @@ namespace CombatlogParser.Tests
         {
             string subevent = "SPELL_PERIODIC_AURA_APPLIED";
 
-            if (ParsingUtil.TryParsePrefixAffixSubevent(subevent, out CombatlogEventPrefix prefix, out CombatlogEventSuffix suffix))
+            if (ParsingUtil.TryParsePrefixAffixSubeventF(subevent, out CombatlogEventPrefix prefix, out CombatlogEventSuffix suffix))
             {
                 Assert.Multiple(() =>
                 {
@@ -83,34 +83,8 @@ namespace CombatlogParser.Tests
         [Test]
         public void SubeventNonCombat()
         {
-            string subevent = "ZONE_CHANGED";
-
-            if(ParsingUtil.TryParsePrefixAffixSubevent(subevent, out CombatlogEventPrefix _, out CombatlogEventSuffix _))
-                Assert.Fail("Allowed invalid combatlog event");
-            else
-                Assert.Pass();
-        }
-
-        [Test]
-        public void FastSubeventPrefSufParse()
-        {
-            string[] subevents = { "SPELL_PERIODIC_AURA_APPLIED", "SPELL_DAMAGE" };
-            
-            CombatlogEventPrefix[] prefixes = new CombatlogEventPrefix[2];
-            CombatlogEventSuffix[] suffixes = new CombatlogEventSuffix[2];
-            for (int i = 0; i < subevents.Length; i++)
-                _ = ParsingUtil.TryParsePrefixAffixSubeventF(subevents[i], out prefixes[i], out suffixes[i]);
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(prefixes[0], Is.EqualTo(CombatlogEventPrefix.SPELL_PERIODIC));
-                Assert.That(prefixes[1], Is.EqualTo(CombatlogEventPrefix.SPELL));
-
-                Assert.That(suffixes[0], Is.EqualTo(CombatlogEventSuffix._AURA_APPLIED));
-                Assert.That(suffixes[1], Is.EqualTo(CombatlogEventSuffix._DAMAGE));
-
-                //Asserts for attempts not needed, as the return is implicit when checking the out parameters.
-            });
+            bool failed = !ParsingUtil.TryParsePrefixAffixSubeventF("ZONE_CHANGED", out _, out _);
+            Assert.That(failed);
         }
     }
 }
