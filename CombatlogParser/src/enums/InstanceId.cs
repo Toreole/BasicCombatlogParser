81fc569b41c19721790269b15852ba2325345b58
Toreole <toreole.official@gmail.com>
2023-03-07 16:45:54 +0100

    boring unnecessary enums

---
+++ CombatlogParser/src/enums/InstanceId.cs
@@ -0,0 +1,33 @@
+﻿namespace CombatlogParser
+{
+    public enum InstanceId
+    {
+        UNKNOWN = 0,
+        //starting with shadowlands because im using Sanctum of Domination for testing sometimes.
+        Castle_Nathria = 2296,
+        Sanctum_of_Domination = 2450,
+        Sepulcher_of_the_First_Ones = 2481,
+
+        Sanguine_Depths = 2284,
+        Spires_of_Ascension = 2285,
+        Necrotic_Wake = 2286,
+        Halls_of_Atonement = 2287,
+        Plaguefall = 2289,
+        Mists_of_Tirna_Scithe = 2290,
+        De_Other_Side = 2291,
+        Theater_Of_Pain = 2293,
+        Tazavesh_the_Veiled_Market = 2441,
+
+        //Dragonflight
+        Vault_of_the_Incarnates = 2522,
+
+        Uldaman_Legacy_of_Tyr = 2451,
+        Azure_Vault = 2515,
+        Nokhud_Offensive = 2516,
+        Neltharus = 2519,
+        Brackenhide_Hollow = 2520,
+        Ruby_Life_Pools = 2521,
+        Algethar_Academy = 2526,
+        Halls_of_Infusion = 2527
+    }
+}
