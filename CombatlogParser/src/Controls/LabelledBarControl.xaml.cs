81c6d951141e607e3a03a198b8f700922674551e
Toreole <toreole.official@gmail.com>
2023-04-07 21:17:06 +0200

    Debug Version of Dmg Overview DataGrid
    More of an experiment than anything else, but this should provide a solid knowledge base for me to work on to be able to smoothly implement it for realsies in the encounter breakdown view.

--- CombatlogParser/src/Controls/LabelledBarControl.xaml.cs
+++ CombatlogParser/src/Controls/LabelledBarControl.xaml.cs
@@ -1,15 +1,100 @@
-﻿using System.Windows.Controls;
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
-namespace CombatlogParser.src.Controls
+namespace CombatlogParser.Controls
 {
     /// <summary>
     /// Interaction logic for LabelledBarControl.xaml
     /// </summary>
     public partial class LabelledBarControl : UserControl
     {
+        public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register(
+            nameof(Maximum), 
+            typeof(double), 
+            typeof(LabelledBarControl),
+            new UIPropertyMetadata(1.0d, new PropertyChangedCallback(OnMaximumChanged)));
+
+        private static void OnMaximumChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if(d is LabelledBarControl bar) 
+            { 
+                bar.Maximum = (double)e.NewValue;
+            }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value), 
+            typeof(double), 
+            typeof(LabelledBarControl),
+            new UIPropertyMetadata(0.0d, new PropertyChangedCallback(OnValueChanged)));
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is LabelledBarControl bar)
+            {
+                bar.Value = (double)e.NewValue;
+            }
+        }
+
+        public static readonly DependencyProperty FillColorProperty = DependencyProperty.Register(
+            nameof(FillColor),
+            typeof(Brush),
+            typeof(LabelledBarControl),
+            new UIPropertyMetadata(Brushes.Green, new PropertyChangedCallback(OnForegroundChanged)));
+
+        public static readonly DependencyProperty TextColorPoperty = DependencyProperty.Register(
+            "TextColor",
+            typeof(Brush),
+            typeof(LabelledBarControl),
+            new UIPropertyMetadata(Brushes.Black, new PropertyChangedCallback(OnTextColorChanged)));
+
+        private static void OnForegroundChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is LabelledBarControl bar)
+            {
+                bar.FillColor = (Brush)e.NewValue;
+            }
+        }
+
+        private static void OnTextColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is LabelledBarControl bar)
+            {
+                bar.AmountLabel.Foreground = (Brush)e.NewValue;
+            }
+        }
+
+        public double Maximum { 
+            get => FillBar.Maximum; 
+            set => FillBar.Maximum = value; 
+        }
+        public double Value
+        {
+            get => FillBar.Value;
+            set
+            {
+                FillBar.Value = value;
+                AmountLabel.Content = value.ToString("0.0");
+            }
+        }
+
         public LabelledBarControl()
         {
             InitializeComponent();
+        }
+
+        public Brush FillColor
+        {
+            get => FillBar.Foreground;
+            set => FillBar.Foreground = value;
+        }
+
+        public Brush TextColor
+        {
+            get => AmountLabel.Foreground;
+            set => AmountLabel.Foreground = value;
         }
     }
 }
---
+++ CombatlogParser/src/Controls/TestList.xaml.cs
@@ -0,0 +1,66 @@
+﻿using CombatlogParser.Controls;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CombatlogParser.src.Controls
+{
+    /// <summary>
+    /// Interaction logic for TestList.xaml
+    /// </summary>
+    public partial class TestList : UserControl
+    {
+        public TestList()
+        {
+            InitializeComponent();
+
+            AddRandomSampleData();
+        }
+
+        private void AddRandomSampleData()
+        {
+            Random random = new();
+            //warning: highly illegal code
+            byte[] objects = new byte[10];
+            var classes = Enum.GetValues<ClassId>();
+            var playerPerformances = objects.Select(x =>
+            {
+                double num = random.NextDouble() * 10000.0d;
+                return new TestData()
+                {
+                    Name = "Player",
+                    Value = num,
+                    Maximum = 10000.0,
+                    ValueString = num.ToString("0.0"),
+                    Color = classes[random.Next(1, classes.Length)].GetClassBrush(),
+                };
+            }).OrderByDescending(x => x.Value).ToArray();
+            var max = playerPerformances.Max(x => x.Value);
+
+            foreach (var x in playerPerformances) {
+                x.Maximum = max;
+                DataGrid.Items.Add(x);
+            }
+
+        }
+
+        void ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button b)
+            {
+                object arg = b.Tag;
+                e.Handled = true;
+            }
+        }
+
+        public class TestData 
+        {
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Maximum { get; set; }
+            public string ValueString { get; set; }
+            public Brush Color { get; set; }
+        }
+    }
+}
