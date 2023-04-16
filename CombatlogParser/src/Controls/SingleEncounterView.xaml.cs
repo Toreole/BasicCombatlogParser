900cfa79f0694ba03ed3b806d310b766273ec26a
Toreole <toreole.official@gmail.com>
2023-04-16 18:59:19 +0200

    Started some work on EncounterView.
    Yes that menu band is 99% just copied from Warcraftlogs. Please dont sue me.
    It is very nice looking tho

---
+++ CombatlogParser/src/Controls/SingleEncounterView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CombatlogParser.Controls
+{
+    /// <summary>
+    /// Interaction logic for SingleEncounterView.xaml
+    /// </summary>
+    public partial class SingleEncounterView : UserControl
+    {
+        private Button highlightedButton;
+        public SingleEncounterView()
+        {
+            InitializeComponent();
+            highlightedButton = DamageButton;
+        }
+
+        private void TabButtonClick(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            highlightedButton.Style = this.Resources["selection-button-spaced"] as Style;
+            highlightedButton = (Button)sender;
+            object tag = highlightedButton.Tag;
+            highlightedButton.Style = this.Resources["selection-button-spaced-highlighted"] as Style;
+        }
+    }
+}
